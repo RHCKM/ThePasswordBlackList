0bd7cb54ce7464a890aa6152d404221365c826ae
Robert Hunter <robert.hunter@cookmedical.com>
2015-08-28 00:41:14 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+function jsonTask (){
+            console.log('test2');
+            var jsonSrc = "passwords.json";
+            $.getJSON(jsonSrc,function () {
+                console.log("test3");
+            })
+            .done(function(data){
+                console.log("Fetch Success");
+                var passwords = Object.keys(data['passwords']).sort();
+                console.log('Number Of Passwords: ' + passwords.length);
+
+                var passwordsHtml = [];
+
+                $.each(passwords,function(i,val){
+                    var passVal = val.replace(/\s+/g, '');
+                    passwordsHtml.push("<li><a href='#'>" + passVal + "</a></li>");
+                });
+
+                $("#listCustom").append(passwordsHtml).listview('refresh');
+            })
+            .fail(function(){
+                console.log('Fetch Fail');
+            });
+        };
